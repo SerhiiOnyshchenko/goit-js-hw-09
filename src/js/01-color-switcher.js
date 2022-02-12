70e2dc81807af651efe9577331069035844a9cbb
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-12 13:42:57 +0200

    dz 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+const ref = {
+  bodyEl: document.querySelector('body'),
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+let timer = null
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+const  changeColorBGBody = () =>{
+	ref.bodyEl.style.background = getRandomHexColor();
+}
+
+ref.startBtn.addEventListener('click', () => {
+   timer = setInterval(changeColorBGBody, 1000);
+	ref.startBtn.disabled = true;
+})
+
+ref.stopBtn.addEventListener('click', () => {
+   clearInterval(timer) 
+	ref.startBtn.disabled = false;
+})
