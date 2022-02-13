9509e2d0846af86c7d4e469201163e2e0b934891
Serhii Onyshchenko <vkhellboy1992@gmail.com>
2022-02-13 13:08:57 +0200

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,9 +1,28 @@
+// all modules
+import Notiflix from 'notiflix';
+
+const refs = {
+	form: document.querySelector('.form'),
+	delayInput: document.querySelector('input[name="delay"]'),
+	stepInput: document.querySelector('input[name="step"]'),
+	amountInput: document.querySelector('input[name="amount"]'),
+};
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
     // Fulfill
+	 Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
   } else {
     // Reject
+	 Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
   }
 }
 
+refs.form.addEventListener('submit', (e) => {
+e.preventDefault()
+const position = refs.amountInput.value
+const delay = refs.delayInput.value;
+createPromise(position,delay)
+console.log(refs.delayInput.value,refs.amountInput.value,refs.stepInput.value);
+})
